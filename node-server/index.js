db7fb494191500b87789adf2db461de405d3b6b0
parthgala1 <parth.gala1356@gmail.com>
2025-02-01 17:43:34 +0530

    env changes

--- node-server/index.js
+++ node-server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -22,7 +22,6 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "100mb" }));
 app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
-
 
 const PORT = process.env.PORT || 4224;
 
